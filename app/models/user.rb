e33f1dc92227b086046f9f70d76c1ff4fc10782c
khinmarlaroo <khinmarlar19793@gmail.com>
2021-05-17 15:53:15 +0630

    fixed user update error

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   belongs_to :created_user, class_name: "User", foreign_key: "create_user_id"
   belongs_to :updated_user, class_name: "User", foreign_key: "updated_user_id"
 
-  validates :name, :password_confirmation, :profile, presence: true
+  validates :name, :profile, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   
   has_secure_password
