ad81304624ebaed66854a8c9d999e4e07d6030f4
khinmarlaroo <khinmarlar19793@gmail.com>
2021-05-17 11:46:18 +0630

    add validation for user create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   belongs_to :created_user, class_name: "User", foreign_key: "create_user_id"
   belongs_to :updated_user, class_name: "User", foreign_key: "updated_user_id"
+
+  validates :name, :password_confirmation, :profile, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   
   has_secure_password
 
